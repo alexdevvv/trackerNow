4925d0f911a11077f8cda1a949d8a273e4cb5d30
Alex Sergeev <alex.dev.sergeev@gmail.com>
2021-04-08 16:03:37 +0300

    2. Репозиторий под проект Tracker [#481216]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -14,4 +14,17 @@ public class TrackerTest {
         Item result = tracker.findById(item.getId());
         assertThat(result.getName(), is(item.getName()));
     }
+
+    @Test
+    public void whenAddNew2ItemThenTracker() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item();
+        Item item2 = new Item();
+        item1.setName("item1");
+        item2.setName("item2");
+        tracker.add(item1);
+        tracker.add(item2);
+        Item rsl1 = tracker.findById(item2.getId());
+        assertThat(rsl1.getName(), is(item2.getName()));
+    }
 }
